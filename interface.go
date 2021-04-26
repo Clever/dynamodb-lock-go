9d15ac0055713fc4f7e205f831c9596629a96c14
Taylor Sutton <taylor.sutton@clever.com>
2021-04-26 15:16:59 -0700

    Add Go code

---
+++ interface.go
@@ -0,0 +1,129 @@
+package lock
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"gopkg.in/Clever/kayvee-go.v6/logger"
+)
+
+// Locker is an interface for acquiring and releasing locks on keys with support for automatic lock expiration via leases.
+// Locker knows about three kinds of resources: keys, owners, and locks.
+// A key is just a string, as far as locker is concerned. A client can attach its own meaning to the key.
+// Likewise an owner is just a string. A client can attach its own meaning and format to owner strings, and should ensure different logical units (goroutines or whatnot) have different owner strings.
+// A lock is a representation of exclusive access for a particular owner to a particular key. It has its own type: Lock.
+// All locks have an end time at which they are considered no longer valid. The period of time in which a lock is valid is called the "lease."
+// Leases prevent a lock from being held indefinitely by an owner which has crashed or otherwise finished without releasing the lock.
+// Leases are stored as a specific point in time. Clients are responsible for ensuring that their clocks are synchronized; a small grace period allows small (~seconds) desyncs.
+// The authoritative way to determine if a lock is still valid is to talk to the server by attempting to heartbeat the lock.
+type Locker interface {
+	// AcquireLock attepts to get the lock for a given input key. A lock can be acquired if there is not an active lease by a different owner.
+	// If there is an active lease by the same owner as provided in the input, Acquire will succeed and the active lease will be replaced by input's lease.
+	// Key and Owner are required inputs, as well as exactly one of LeaseEnd or LeaseDuration
+	// If LeaseDuration is provided, it will be converted into a LeaseEnd before sending the request to the backend.
+	// Returns either a non-nil Lock and a nil error, or a nil lock and non-nil error.
+	// The error will be of type UnavailableError when there is an active lease on the key by a different owner.
+	// Other errors may indicate e.g. a network problem talking to the backend, a validation error in the input, etc.
+	AcquireLock(ctx context.Context, input AcquireLockInput) (*Lock, error)
+
+	// AcquireAnyLock is a thin wrapper around AcquireLock for attepting to lock any one of a list of keys.
+	// Underneath, it simply attempts to lock them one at a time and returns the first successful lock or an error.
+	// It returns UnavailableError only when every single key was unavailable.
+	AcquireAnyLock(ctx context.Context, input AcquireAnyLockInput) (*Lock, error)
+
+	// HeartbeatLock is similar to AcquireLock except that it also fails if another owner held the lock in between acquiring the original lock and now, even if they have since released it.
+	// In particular, Heartbeat succeeds in refreshing the lock if the lock's lease has expired but no one else used the lock in the mean time.
+	HeartbeatLock(ctx context.Context, input HeartbeatLockInput) (*Lock, error)
+
+	// StartBackgroundHeartbeats starts a goroutine to heartbeat the lock at set intervals.
+	// It can be used to extend a lease during a long-running execution.
+	// It stops if its context is cancelled, or if it gets an error. It reports the error via returned channel.
+	// The lock pointer passed to it will be updated each time the heartbeat succeeds.
+	// Since the lock isn't behind a mutex, take care if updating it manually after calling this.
+	StartBackgroundHeartbeats(ctx context.Context, lockPointer *Lock, heartbeatInterval time.Duration, leaseDuration time.Duration) <-chan error
+
+	// A lock can be released so long as it isn't owned by someone else i.e. if lock.Owner could acquire the lock.
+	// That means that is is NOT an error to release a lock that wasn't owned by anyone.
+	// An error means that the lock was NOT released. Either it is currently held by someone else, or there was a system error trying to release the lock.
+	// If the lock is held by someone else, that's generally not retryable, and that is represented by the error type UnavailableError. In this case, the caller can be certain that the caller does not still hold the lock.
+	// Any other error type could indicate that caller still holds the lock e.g. if there were a dynamo system error.
+	ReleaseLock(ctx context.Context, lock Lock) error
+}
+
+// Lock is a representation of a row in the lock table.
+type Lock struct {
+	// Note: these struct tags need to match the values for attribute names in lock.go
+	Key         string    `dynamodbav:"key"`
+	Owner       string    `dynamodbav:"owner"`
+	LeasedUntil time.Time `dynamodbav:"leasedUntil"`
+	TTL         time.Time `dynamodbav:"ttl"`
+}
+
+// NewLocker returns a Locker which talks to the given DynamoDB table using the given DynamoDBAPI, and logs to the given logger.
+func NewLocker(ddbAPI dynamodbiface.DynamoDBAPI, tableName string, logger logger.KayveeLogger) Locker {
+	return &locker{
+		dynamoDBAPI: ddbAPI,
+		tableName:   tableName,
+		logger:      logger,
+	}
+}
+
+// UnavailableError represents an attempt to get or update a lock has a different owner
+type UnavailableError struct {
+	Err error
+}
+
+func (l UnavailableError) Error() string {
+	return fmt.Sprintf("lock unavailable: %v", l.Err)
+}
+
+// Unwrap lets this work with errors.Is and errors.As
+func (l UnavailableError) Unwrap() error {
+	return l.Err
+}
+
+// AcquireLockInput is the input to AcquireLock
+type AcquireLockInput struct {
+	// Key is required
+	Key string
+	// Owner is required
+	Owner string
+
+	// Exactly one of LeaseEnd and LeaseDuration should be provided, the other nil
+	// LeaseEnd is the time at which the lease should expire.
+	LeaseEnd *time.Time
+	// LeaseDuration is a convenience field that will be converted into a LeaseEnd by adding it to time.Now() before sending to the backend
+	LeaseDuration *time.Duration
+}
+
+// AcquireAnyLockInput is the input to AcquireLock
+type AcquireAnyLockInput struct {
+	// Keys is required
+	Keys []string
+	// Owner is Required
+	Owner string
+
+	// RandomizeOrder is optional (defaults to false). If true, Locker will attempt to lock each key in a random order.
+	RandomizeOrder *bool
+
+	// Exactly one of LeaseEnd and LeaseDuration should be provided, the other nil
+	// LeaseEnd is the time at which the lease should expire.
+	LeaseEnd *time.Time
+	// LeaseDuration is a convenience field that will be converted into a LeaseEnd by adding it to time.Now() before sending to the backend
+	LeaseDuration *time.Duration
+}
+
+// HeartbeatLockInput is the input to AcquireLock
+type HeartbeatLockInput struct {
+	// Note that the LeasedUntil field is ignored, but Owner and Key must be correct.
+	OriginalLock Lock
+
+	// Exactly one of LeaseEnd and LeaseDuration should be provided, the other nil
+	// They work the same as in AcquireLockInput
+	// LeaseEnd is the time at which the lease should expire.
+	LeaseEnd *time.Time
+	// LeaseDuration is a convenience field that will be converted into a LeaseEnd by adding it to time.Now() before sending to the backend
+	LeaseDuration *time.Duration
+}
---
+++ lock.go
@@ -0,0 +1,307 @@
+package lock
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+	"github.com/hashicorp/go-multierror"
+	"gopkg.in/Clever/kayvee-go.v6/logger"
+)
+
+const gracePeriod = 5 * time.Second
+const ttlDurationAfterLease = 2 * 24 * time.Hour
+
+const (
+	// the values of these need to match struct tag for Lock
+	leaseEndAttribute = "leasedUntil"
+	partitionKeyName  = "key"
+)
+
+type locker struct {
+	dynamoDBAPI dynamodbiface.DynamoDBAPI
+	tableName   string
+	logger      logger.KayveeLogger
+}
+
+func (l locker) canAcquireLockExpression(owner string, leaseExpiredBefore time.Time) expression.Builder {
+	// either:
+	// - no item with PK exists
+	// - item exists but lease < specified leaseExpiredBefore time
+	// - item exists, and we own it
+
+	doesntExist := expression.Name(partitionKeyName).AttributeNotExists()
+	expired := expression.Name(leaseEndAttribute).LessThan(expression.Value(leaseExpiredBefore))
+	ownedByUs := expression.Name("owner").Equal(expression.Value(owner))
+
+	return expression.NewBuilder().WithCondition(expression.Or(doesntExist, expired, ownedByUs))
+}
+
+func (l locker) canHeartbeatLockExpression(owner string, leaseExpiredBefore time.Time) expression.Builder {
+	// Lock must exist and be owned by us
+	// Lease does not matter
+
+	ownedByUs := expression.Name("owner").Equal(expression.Value(owner))
+
+	return expression.NewBuilder().WithCondition(ownedByUs)
+}
+
+func (l locker) AcquireLock(ctx context.Context, input AcquireLockInput) (*Lock, error) {
+	if input.LeaseDuration != nil && input.LeaseEnd != nil {
+		return nil, fmt.Errorf("one of LeaseDuration or leaseEnd is required")
+	}
+
+	var leaseEnd time.Time
+	if input.LeaseEnd != nil {
+		leaseEnd = *input.LeaseEnd
+	} else if input.LeaseDuration != nil {
+		leaseEnd = time.Now().Add(*input.LeaseDuration)
+	}
+
+	newLock := Lock{
+		Owner:       input.Owner,
+		Key:         input.Key,
+		LeasedUntil: leaseEnd,
+		TTL:         leaseEnd.Add(ttlDurationAfterLease),
+	}
+
+	err := l.putLockIfAble(ctx, newLock)
+	if err != nil {
+		return nil, err
+	}
+
+	return &newLock, nil
+}
+
+func (l locker) AcquireAnyLock(ctx context.Context, input AcquireAnyLockInput) (*Lock, error) {
+	var errs error
+
+	keys := input.Keys
+	if input.RandomizeOrder != nil && *input.RandomizeOrder {
+		keys = randomizedCopy(input.Keys)
+	}
+
+	allLocksUnavailable := true
+	for _, key := range keys {
+		lock, err := l.AcquireLock(ctx, AcquireLockInput{
+			Owner:         input.Owner,
+			Key:           key,
+			LeaseDuration: input.LeaseDuration,
+			LeaseEnd:      input.LeaseEnd,
+		})
+		if err == nil {
+			return lock, nil
+		}
+		if !errors.As(err, &UnavailableError{}) {
+			allLocksUnavailable = false
+		}
+		errs = multierror.Append(errs, fmt.Errorf("locking key %s: %w", key, err))
+	}
+	if allLocksUnavailable {
+		errs = UnavailableError{Err: errs}
+	}
+	return nil, errs
+}
+
+func (l locker) HeartbeatLock(ctx context.Context, input HeartbeatLockInput) (*Lock, error) {
+	if input.LeaseDuration != nil && input.LeaseEnd != nil {
+		return nil, fmt.Errorf("both LeaseDuration and leaseEnd were non-nil")
+	}
+
+	var leaseEnd time.Time
+	if input.LeaseEnd != nil {
+		leaseEnd = *input.LeaseEnd
+	} else if input.LeaseDuration != nil {
+		leaseEnd = time.Now().Add(*input.LeaseDuration)
+	}
+
+	newLock := Lock{
+		Owner:       input.OriginalLock.Owner,
+		Key:         input.OriginalLock.Key,
+		LeasedUntil: leaseEnd,
+		TTL:         leaseEnd.Add(ttlDurationAfterLease),
+	}
+
+	item, err := dynamodbattribute.MarshalMap(newLock)
+	if err != nil {
+		return nil, fmt.Errorf("failed to DynamoDB marshal Record, %v", err)
+	}
+
+	// In order to work around clock desynchronization a little, we allow a grace period:
+	//   we'll only take a lease if it's been expired for gracePeriod duration
+	leaseExpiredBefore := time.Now().Add(-gracePeriod)
+
+	canHeartbeatExpression, err := l.canHeartbeatLockExpression(input.OriginalLock.Owner, leaseExpiredBefore).Build()
+	if err != nil {
+		return nil, fmt.Errorf("building condition expression to acquire lock: %v", err)
+	}
+	putInput := dynamodb.PutItemInput{
+		TableName:                 aws.String(l.tableName),
+		ConditionExpression:       canHeartbeatExpression.Condition(),
+		ExpressionAttributeNames:  canHeartbeatExpression.Names(),
+		ExpressionAttributeValues: canHeartbeatExpression.Values(),
+		Item:                      item,
+	}
+
+	l.logger.DebugD("dynamo-operation", logger.M{
+		"op":    "PutItem",
+		"input": putInput,
+	})
+
+	_, err = l.dynamoDBAPI.PutItemWithContext(ctx, &putInput)
+
+	var ccfe *dynamodb.ConditionalCheckFailedException
+	if errors.As(err, &ccfe) {
+		return nil, UnavailableError{Err: ccfe}
+	} else if err != nil {
+		l.logger.ErrorD("unexpected-dynamo-error", logger.M{
+			"error":   err,
+			"message": err.Error(),
+			"type":    fmt.Sprintf("%T", err),
+			"op":      "PutItem",
+			"input":   putInput,
+		})
+		return nil, err
+	}
+	return &newLock, nil
+}
+
+func (l locker) ReleaseLock(ctx context.Context, lock Lock) error {
+	return l.deleteLockIfAble(ctx, Lock{
+		Key:   lock.Key,
+		Owner: lock.Owner,
+	})
+}
+
+func (l locker) putLockIfAble(ctx context.Context, lock Lock) error {
+	item, err := dynamodbattribute.MarshalMap(lock)
+	if err != nil {
+		return fmt.Errorf("failed to DynamoDB marshal Record, %v", err)
+	}
+
+	// In order to work around clock desynchronization a little, we allow a grace period:
+	//   we'll only take a lease if it's been expired for gracePeriod duration
+	leaseExpiredBefore := time.Now().Add(-gracePeriod)
+
+	canAcquireExpression, err := l.canAcquireLockExpression(lock.Owner, leaseExpiredBefore).Build()
+	if err != nil {
+		return fmt.Errorf("building condition expression to acquire lock: %v", err)
+	}
+	putInput := dynamodb.PutItemInput{
+		TableName:                 aws.String(l.tableName),
+		ConditionExpression:       canAcquireExpression.Condition(),
+		ExpressionAttributeNames:  canAcquireExpression.Names(),
+		ExpressionAttributeValues: canAcquireExpression.Values(),
+		Item:                      item,
+	}
+
+	l.logger.DebugD("dynamo-operation", logger.M{
+		"op":    "PutItem",
+		"input": putInput,
+	})
+
+	_, err = l.dynamoDBAPI.PutItemWithContext(ctx, &putInput)
+
+	var ccfe *dynamodb.ConditionalCheckFailedException
+	if errors.As(err, &ccfe) {
+		return UnavailableError{Err: ccfe}
+	} else if err != nil {
+		l.logger.ErrorD("unexpected-dynamo-error", logger.M{
+			"error":   err,
+			"message": err.Error(),
+			"type":    fmt.Sprintf("%T", err),
+			"op":      "PutItem",
+			"input":   putInput,
+		})
+		return err
+	}
+
+	return nil
+}
+
+func (l locker) deleteLockIfAble(ctx context.Context, lock Lock) error {
+
+	// In order to work around clock desynchronization a little, we allow a grace period:
+	//   we'll only take a lease if it's been expired for gracePeriod duration
+	leaseExpiredBefore := time.Now().Add(-gracePeriod)
+
+	canAcquireExpression, err := l.canAcquireLockExpression(lock.Owner, leaseExpiredBefore).Build()
+	if err != nil {
+		return fmt.Errorf("building condition expression to acquire lock: %v", err)
+	}
+	deleteInput := dynamodb.DeleteItemInput{
+		TableName:                 aws.String(l.tableName),
+		ConditionExpression:       canAcquireExpression.Condition(),
+		ExpressionAttributeNames:  canAcquireExpression.Names(),
+		ExpressionAttributeValues: canAcquireExpression.Values(),
+		Key: map[string]*dynamodb.AttributeValue{
+			partitionKeyName: &dynamodb.AttributeValue{
+				S: &lock.Key,
+			},
+		},
+	}
+
+	l.logger.DebugD("dynamo-operation", logger.M{
+		"op":    "DeleteItem",
+		"input": deleteInput,
+	})
+
+	_, err = l.dynamoDBAPI.DeleteItemWithContext(ctx, &deleteInput)
+
+	var ccfe *dynamodb.ConditionalCheckFailedException
+	if errors.As(err, &ccfe) {
+		return UnavailableError{Err: ccfe}
+	} else if err != nil {
+		l.logger.ErrorD("unexpected-dynamo-error", logger.M{
+			"error":   err,
+			"message": err.Error(),
+			"type":    fmt.Sprintf("%T", err),
+			"op":      "PutItem",
+			"input":   deleteInput,
+		})
+		return err
+	}
+
+	return nil
+}
+
+func (l locker) StartBackgroundHeartbeats(ctx context.Context, lock *Lock, heartbeatInterval time.Duration, leaseDuration time.Duration) <-chan error {
+	errChan := make(chan error)
+	go func() {
+		ticker := time.NewTicker(heartbeatInterval)
+		defer ticker.Stop()
+		select {
+		case <-ticker.C:
+			newLock, err := l.HeartbeatLock(ctx, HeartbeatLockInput{
+				OriginalLock:  *lock,
+				LeaseDuration: &leaseDuration,
+			})
+			if err != nil {
+				errChan <- fmt.Errorf("heartbeat error: %w", err)
+				return
+			}
+			*lock = *newLock
+		case <-ctx.Done():
+			return
+		}
+	}()
+	return errChan
+}
+
+func randomizedCopy(keys []string) []string {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	randomizedKeys := make([]string, len(keys))
+	copy(randomizedKeys, keys)
+
+	r.Shuffle(len(keys), func(i, j int) {
+		randomizedKeys[i], randomizedKeys[j] = randomizedKeys[j], randomizedKeys[i]
+	})
+	return randomizedKeys
+}
